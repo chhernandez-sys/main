bcc6449dd3a3b48fd50a06f6992cd324e9307024
chhernandez-sys <58121679+chhernandez-sys@users.noreply.github.com>
2026-04-29 12:10:46 -0600

    Merge pull request #13 from chhernandez-sys/test12-main
    test 12
    # Conflicts:
    #	Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 using System;
 
+<<<<<<< HEAD
 Console.WriteLine("hola nuevo mundo");
+=======
+Console.WriteLine("hola hola viejo branch 12");
+>>>>>>> 343b2a3 (Merge pull request #13 from chhernandez-sys/test12-main)
