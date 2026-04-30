4665f5978b4fd2c74bc3d828d04b7c7f52810e8a
CristhianHernandez <cristhianhh21@gmail.com>
2026-04-30 13:16:46 -0600

    Cherry-pick with conflicts: bb51db09b314b8a2efa56e47e3c5ac99552a794e

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using System;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 Console.WriteLine("hola nuevo mundo");
 =======
 Console.WriteLine("hola hola viejo branch 13");
 >>>>>>> 6b41f89 (Change to 13)
+=======
+Console.WriteLine("hello old branch 13");
+>>>>>>> bb51db0 (Change to english)
